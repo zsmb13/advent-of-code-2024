886b3d94605425b49b41202043e62b7faca099ca
Márton Braun <marton.braun@jetbrains.com>
2024-12-12 14:44:00 +0100

    Clean up day12

--- src/Day12.kt
+++ src/Day12.kt
@@ -4,126 +4,89 @@ import readInput
 
 data class Coords(val x: Int, val y: Int)
 
-fun part1(input: List<String>): Int {
+operator fun List<String>.get(x: Int, y: Int) = getOrNull(y)?.getOrNull(x)
+operator fun List<String>.get(coords: Coords) = get(coords.x, coords.y)
+
+fun findRegions(
+    input: List<String>,
+    onRegionFound: (plots: MutableSet<Coords>, perimeter: Int) -> Unit,
+) {
     val xRange = input[0].indices
     val yRange = input.indices
 
-    fun get(x: Int, y: Int) = input[y][x]
+    class MutableInt(var value: Int)
 
-    fun visit(visited: MutableSet<Coords>, x: Int, y: Int, char: Char): Int {
-        val dis = Coords(x, y)
-        if (dis in visited) {
-            return 0
+    fun visit(region: MutableSet<Coords>, perimeter: MutableInt, x: Int, y: Int, char: Char) {
+        val current = Coords(x, y)
+        if (current in region) {
+            return
         }
-        if (x !in xRange || y !in yRange || get(x, y) != char) {
-            return 1
+        if (input[current] != char) {
+            perimeter.value++
+            return
         }
-        visited += Coords(x, y)
+        region += current
 
-        val offsets = listOf(
-            0 to 1,
-            0 to -1,
-            1 to 0,
-            -1 to 0,
-        )
-        return offsets.sumOf { (dx, dy) ->
-            visit(visited, x + dx, y + dy, char)
+        listOf(
+            0 to -1,// up
+            0 to 1, // down
+            -1 to 0, // left
+            1 to 0, // right
+        ).forEach { (dx, dy) ->
+            visit(region, perimeter, x + dx, y + dy, char)
         }
     }
 
-    val allVisited = mutableSetOf<Coords>()
-    var sum = 0
+    val visited = mutableSetOf<Coords>()
     for (y in yRange) {
         for (x in xRange) {
-            if (Coords(x, y) !in allVisited) {
-                val visited = mutableSetOf<Coords>()
-                val perimeter = visit(visited, x, y, input[y][x])
+            if (Coords(x, y) !in visited) {
+                val region = mutableSetOf<Coords>()
+                val perimeter = MutableInt(0)
+                visit(region, perimeter, x, y, input[x, y]!!)
 
-                sum += visited.size * perimeter
+                visited.addAll(region)
 
-                allVisited.addAll(visited)
+                onRegionFound(region, perimeter.value)
             }
         }
+    }
+}
+
+fun part1(input: List<String>): Int {
+    var sum = 0
+    findRegions(input) { region, perimeter ->
+        sum += region.size * perimeter
     }
     return sum
 }
 
 fun part2(input: List<String>): Int {
-    val xRange = input[0].indices
-    val yRange = input.indices
-
-    fun get(x: Int, y: Int) = if (x !in xRange || y !in yRange) null else input[y][x]
-
-    fun visit(visited: MutableSet<Coords>, x: Int, y: Int, char: Char) {
-        val dis = Coords(x, y)
-        if (dis in visited) {
-            return
-        }
-        if (x !in xRange || y !in yRange || get(x, y) != char) {
-            return
-        }
-        visited += Coords(x, y)
-
-        val offsets = listOf(
-            0 to 1,
-            0 to -1,
-            1 to 0,
-            -1 to 0,
-        )
-        offsets.forEach { (dx, dy) ->
-            visit(visited, x + dx, y + dy, char)
-        }
-    }
-
     fun Coords.corners(char: Char): Int {
         var c = 0
-
-        // top left, outer then inner
-        if (get(x - 1, y) != char && get(x, y - 1) != char)
-            c++
-        if (get(x - 1, y) == char && get(x, y - 1) == char && get(x - 1, y - 1) != char)
-            c++
-
-        // top right, outer then inner
-        if (get(x + 1, y) != char && get(x, y - 1) != char)
-            c++
-        if (get(x + 1, y) == char && get(x, y - 1) == char && get(x + 1, y - 1) != char)
-            c++
-
-        // bottom left, outer then inner
-        if (get(x - 1, y) != char && get(x, y + 1) != char)
-            c++
-        if (get(x - 1, y) == char && get(x, y + 1) == char && get(x - 1, y + 1) != char)
-            c++
-
-        // bottom right, outer then inner
-        if (get(x + 1, y) != char && get(x, y + 1) != char)
-            c++
-        if (get(x + 1, y) == char && get(x, y + 1) == char && get(x + 1, y + 1) != char)
-            c++
-
+        listOf(
+            -1 to -1, // top left
+            +1 to -1, // top right
+            -1 to +1, // bottom left
+            +1 to +1, // bottom right
+        ).forEach { (dx, dy) ->
+            if (input[x + dx, y] != char && input[x, y + dy] != char)
+                c++ // outer corner
+            if (input[x + dx, y] == char && input[x, y + dy] == char && input[x + dx, y + dy] != char)
+                c++ // inner corner
+        }
         return c
     }
 
-    val allVisited = mutableSetOf<Coords>()
     var sum = 0
-    for (y in yRange) {
-        for (x in xRange) {
-            if (Coords(x, y) !in allVisited) {
-                val visited = mutableSetOf<Coords>()
-                visit(visited, x, y, get(x, y)!!)
-
-                val corners = visited.sumOf { it.corners(get(x, y)!!) }
-
-                sum += visited.size * corners
-
-                allVisited.addAll(visited)
-            }
+    findRegions(input) { region, _ ->
+        val corners = region.sumOf { plot ->
+            plot.corners(input[plot]!!)
         }
+        sum += region.size * corners
     }
     return sum
 }
-
 
 fun main() {
     val input = readInput("Day12")
